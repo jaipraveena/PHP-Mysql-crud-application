f20ca6e90c8fabfef69e96203d89f4732bb3ca56
jaipraveena <150928557+jaipraveena@users.noreply.github.com>
2024-01-31 00:15:39 +0530

    Add files via upload

---
+++ Student application/delete.php
@@ -0,0 +1,26 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","");
+
+$db = mysqli_select_db($connection,"dbcrud");
+$delete = $_GET['del'];
+
+
+$sql = "delete from registration where id = '$delete'";
+
+
+if(mysqli_query($connection,$sql))
+           {
+
+            echo '<script> location.replace("index.php")</script>';  
+           }
+           else
+           {
+           echo "Some thing Error" . $connection->error;
+
+           }
+
+?>
+
+
+
